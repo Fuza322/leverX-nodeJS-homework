0cb4b98c8ddffd1bee5b6c226baa5cbe1686472a
Fuza322 <vanya.osipik322@gmail.com>
2021-02-24 14:53:51 +0300

    test

--- 01/Task9.js
+++ 01/Task9.js
@@ -1,5 +1,1 @@
-function flatAndSort(arr) {
-  return arr.flat(Infinity).sort( (a, b) => a - b)
-}
-
-console.log(flatAndSort([1, 2, 1000, 300, [400, [3, 10, [11, 12]], [1, 2, [3, 4]], 5, 6]]))
+// some code
